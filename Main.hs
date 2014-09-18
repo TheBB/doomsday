84acc2e9d5e493e288977ad6fa6f0deef806f033
Eivind Fonn <evfonn@gmail.com>
2014-09-18 16:55:14 +0200

    Collect session stats.

--- Main.hs
+++ Main.hs
@@ -1,15 +1,18 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
+import Data.Char (toLower)
 import Data.Functor ((<$>))
 import Data.Time
 import System.IO (hFlush, stdout)
 import System.Random (getStdRandom, randomR)
 
 newtype WeekDay = WeekDay Integer
-                  deriving (Eq, Ord, Enum, Num)
+                  deriving (Ord, Enum, Num)
 
 [monday, tuesday, wednesday, thursday, friday, saturday, sunday] = [WeekDay 0 .. WeekDay 6]
 
+instance Eq WeekDay where
+  WeekDay n == WeekDay m = n `mod` 7 == m `mod` 7
 
 instance Show WeekDay where
   show (WeekDay n)
@@ -20,6 +23,19 @@ instance Show WeekDay where
     | n `mod` 7 == 4 = "Friday"
     | n `mod` 7 == 5 = "Saturday"
     | n `mod` 7 == 6 = "Sunday"
+
+readDay :: String -> Maybe WeekDay
+readDay str
+  | istr `elem` ["m", "mon", "monday"] = Just monday
+  | istr `elem` ["tue", "tuesday"] = Just tuesday
+  | istr `elem` ["w", "wed", "wednesday"] = Just wednesday
+  | istr `elem` ["thu", "thursday"] = Just thursday
+  | istr `elem` ["f", "fri", "friday"] = Just friday
+  | istr `elem` ["sat", "saturday"] = Just saturday
+  | istr `elem` ["sun", "sunday"] = Just sunday
+  | otherwise = Nothing
+  where istr = map toLower str
+
 
 yearRange = 400
 dayRange = 365 * yearRange
@@ -49,10 +65,32 @@ weekDay day = doomsDay day + WeekDay (day `diffDays` root)
   where (year, _, _) = toGregorian day
         root = fromGregorian year 1 (if isLeapYear year then 4 else 3)
 
-main = do
+getDay :: IO WeekDay
+getDay = do
+  input <- getLine
+  case readDay input of
+   Just day -> return day
+   _ -> getDay
+
+runTest :: Integer -> Integer -> IO ()
+runTest total yay = do
+  putStrLn $ show yay ++ "/" ++ show total
+  
   day <- randomDate
-  putStr . show $ day
+  putStr . (++ "? ") . show $ day
   hFlush stdout
-  _ <- getLine
-  print . weekDay $ day
-  main
+
+  guess <- getDay
+  case guess == weekDay day of
+   True -> putStrLn "Correct!" >> next (yay + 1)
+   False -> putStrLn ("Wrong! Correct was " ++ show (weekDay day)) >> next yay
+   where next = runTest (total + 1)
+
+
+main = runTest 0 0
+  -- day <- randomDate
+  -- putStr . show $ day
+  -- hFlush stdout
+  -- _ <- getLine
+  -- print . weekDay $ day
+  -- main
